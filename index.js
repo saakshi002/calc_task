7fd32ac0408fb96f69ab75136048834246a57cce
saakshi002 <92668132+saakshi002@users.noreply.github.com>
2021-10-17 22:35:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'Del') {
+            screen.value = screen.value.slice(0, -1);
+            screenValue = screen.value;
+        }
+        else if (buttonText == '=') {
+            h = screen.value + '=' + eval(screenValue);
+            screen.value = eval(screenValue);
+        }
+        else if (buttonText == 'sqrt') {
+            screenValue = Math.sqrt(screenValue);
+            screen.value = screenValue;
+        }
+
+        else if (buttonText == 'htr') {
+            screen.value = h;
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+
+
+    })
+}
+
